cc2a5aacceb9936173ccc94d7fbd3e7a4c02bc3e
DvDCoder7 <davidicheng@usp.br>
2026-06-23 17:53:27 -0300

    Hehe

---
+++ README.md
@@ -0,0 +1,5 @@
+# Treinando Grafos para CP (Prog Competitiva)
+Link do grupo do codeforces:
+https://codeforces.com/group/ibNhxWfOek/contests
+
+Template que uso está em `template.cpp`
---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define MAXN ((int) 2*1e6)
+#define vi vector<int>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
